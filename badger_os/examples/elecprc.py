e1e0cf80f168197d945e87ace0d421833d0daacf
Niels Elgaard Larsen <elgaard@agol.dk>
2023-10-14 02:58:59 +0200

    fix whitespace

--- badger_os/examples/elecprc.py
+++ badger_os/examples/elecprc.py
@@ -10,37 +10,39 @@
 # only shows Danish prices, but similar API's probably exists in many other countries
 
 import badger2040
+import urequests
 
 print("start el prices")
 b = badger2040.Badger2040()
-numview=False
+numview = False
 b.clear()
-b.text("ELECTRICITY PRICES DK EAST", 10,30)
-b.text("staring network", 10,70)
-import urequests
+b.text("ELECTRICITY PRICES DK EAST", 10, 30)
+b.text("staring network", 10, 70)
 b.connect()
 
-URL="https://api.energidataservice.dk/dataset/Elspotprices?offset=0&start=now-P0DT1H&timezone=dk&filter=%7B%22PriceArea%22:[%22DK2%22]%7D&sort=HourUTC%20ASC"
-def pr(s,iv=False):
-    global x,y,sp
+URL = "https://api.energidataservice.dk/dataset/Elspotprices?offset=0&start=now-P0DT1H&timezone=dk&filter=%7B%22PriceArea%22:[%22DK2%22]%7D&sort=HourUTC%20ASC"
+
+
+def pr(s, iv=False):
+    global x, y, sp
     if iv:
-        b.rectangle(x, y, sp-3, 18)
+        b.rectangle(x, y, sp - 3, 18)
         b.set_pen(15)
     b.text(s, x, y)
     b.set_pen(0)
     y += 18
-    if y>110:
+    if y > 110:
         y = 0
         x += sp
         sp = 55
 
+
 while True:
-    b.text("READY "+str(numview), 10, 100)
+    b.text("READY " + str(numview), 10, 100)
     if b.pressed(badger2040.BUTTON_A):
         numview = not numview
-        print("ba","nw=",numview)
+        print("ba", "nw=", numview)
     if b.pressed(badger2040.BUTTON_C):
-        print("wokenC")
         break
     r = urequests.get(URL)
     priser = r.json()
@@ -52,33 +54,33 @@ while True:
     if numview:
         y = 0
         sp = 55
-        pr("El Ø",True)
+        pr("El Ø", True)
         i = 0
         for pris in priser["records"]:
             h = pris["HourDK"].split('T')[1].split(":")[0]
             d = pris["HourDK"].split('T')[0].split("-")[2]
-            p = int(pris["SpotPriceDKK"]/10)
+            p = int(pris["SpotPriceDKK"] / 10)
             if d != day:
-                pr("D: "+d,True)
-            pr(h+":"+str(p))
-            if p>99:
+                pr("D= " + d, True)
+            pr(h + ":" + str(p))
+            if p > 99:
                 sp = 68
             i += 1
-            if i>31:
+            if i > 31:
                 break
-            day=d
+            day = d
     else:
         for pris in priser["records"]:
             h = pris["HourDK"].split('T')[1].split(":")[0]
             d = pris["HourDK"].split('T')[0].split("-")[2]
-            p = int(pris["SpotPriceDKK"]/10)
-            b.rectangle(x, min(112, 112-p), 8, abs(p))
+            p = int(pris["SpotPriceDKK"] / 10)
+            b.rectangle(x, min(112, 112 - p), 8, abs(p))
             if d != day:
-                 b.set_pen(5)
-                 b.rectangle(x, 0, 2, 112)
-                 b.set_pen(0)
+                b.set_pen(5)
+                b.rectangle(x, 0, 2, 112)
+                b.set_pen(0)
             if int(h) % 4 == 0:
-                 b.text(h, x, 116)
+                b.text(h, x, 116)
             x += 8
             day = d
         b.set_pen(5)
@@ -89,4 +91,3 @@ while True:
         b.set_pen(0)
     b.update()
     badger2040.sleep_for(60)
-    
