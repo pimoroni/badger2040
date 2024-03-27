abe5c28ead933092ad81cf3773c0b335ebda9d7b
Phil Howard <github@gadgetoid.com>
2024-03-27 15:32:03 +0000

    Badger 2040W: Handle WiFi STAT_CONNECT_FAIL.
    Expose some more NetworkManager config to the BadgerOS connect() method.

--- firmware/PIMORONI_BADGER2040W/lib/badger2040.py
+++ firmware/PIMORONI_BADGER2040W/lib/badger2040.py
@@ -262,10 +262,13 @@ class Badger2040():
         import gc
 
         status_handler = args.get("status_handler", self.status_handler)
+        error_handler = args.get("error_handler", None)
+        client_timeout = args.get("timeout", 60)
+        retries = args.get("retries", 2)
 
         if WIFI_CONFIG.COUNTRY == "":
             raise RuntimeError("You must populate WIFI_CONFIG.py for networking.")
 
-        network_manager = NetworkManager(WIFI_CONFIG.COUNTRY, status_handler=status_handler)
+        network_manager = NetworkManager(WIFI_CONFIG.COUNTRY, client_timeout=client_timeout, status_handler=status_handler, error_handler=error_handler, retries=retries)
         uasyncio.get_event_loop().run_until_complete(network_manager.client(WIFI_CONFIG.SSID, WIFI_CONFIG.PSK))
         gc.collect()
