41105ee5e4703bec161ab81dd8d07e1672ef2edf
Marcin Juszkiewicz <marcin@juszkiewicz.com.pl>
2024-05-09 09:03:21 +0200

    qrgen: loop entries on Up/Down presses
    Up on first == show last
    Down on last == show first
    
    Signed-off-by: Marcin Juszkiewicz <marcin@juszkiewicz.com.pl>

--- badger_os/examples/qrgen.py
+++ badger_os/examples/qrgen.py
@@ -133,11 +133,16 @@ while True:
             if state["current_qr"] > 0:
                 state["current_qr"] -= 1
                 changed = True
+            else:
+                state["current_qr"] = TOTAL_CODES - 1
+            changed = True
 
-        if display.pressed(badger2040.BUTTON_DOWN):
+        elif display.pressed(badger2040.BUTTON_DOWN):
             if state["current_qr"] < TOTAL_CODES - 1:
                 state["current_qr"] += 1
-                changed = True
+            else:
+                state["current_qr"] = 0
+            changed = True
 
     if display.pressed(badger2040.BUTTON_B) or display.pressed(badger2040.BUTTON_C):
         display.set_pen(15)
